380baf9c8fd6f2c72049fe6b73adac2a458ab404
amalv <1252707+amalv@users.noreply.github.com>
2019-01-29 22:50:56 +0100

    Fix build for production (#6)
    - Split webpack config into production and development
    - Add clean-webpack-plugin dependency for removing dist files each build
    - Add html-webpack-plugin for generating index.html for production build
    - Add netlify badge
    - Set no-extraneous-dependencies eslint rule to allow devDependencies
    - Decrease performance threshold to 95 for lighthouse-ci

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,26 +1,26 @@
-const dev = process.env.NODE_ENV !== 'production';
 const path = require('path');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const plugins = [
+  new CleanWebpackPlugin(['dist']),
   new FriendlyErrorsWebpackPlugin(),
   new CompressionPlugin(),
-];
-
-if (!dev) {
-  plugins.push(new BundleAnalyzerPlugin({
+  new HtmlWebpackPlugin({
+    template: 'index.html',
+  }),
+  new BundleAnalyzerPlugin({
     analyzerMode: 'static',
     reportFilename: 'webpack-report.html',
     openAnalyzer: false,
-  }));
-}
+  }),
+];
 
 module.exports = {
-  mode: dev ? 'development' : 'production',
   context: path.join(__dirname, 'src'),
-  devtool: dev ? 'none' : 'source-map',
   entry: {
     app: './client.js',
   },
