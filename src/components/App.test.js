a2631a2ed3b49e95dea32c1b4b5f9920c50cfdfa
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2019-02-05 22:22:11 +0100

    Fix tests in order to support React Router

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render, waitForElement } from 'react-testing-library';
 import { StateMock } from '@react-mock/state';
+import {
+  Router,
+} from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import App from './App';
 
-const renderComponent = ({ title }) => render(
+const renderWithRouter = (
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) => ({
+  ...render(<Router history={history}>{ui}</Router>),
+  history,
+});
+
+const renderComponent = ({ title }) => renderWithRouter(
   <StateMock state={{ title }}>
     <App />
   </StateMock>,
