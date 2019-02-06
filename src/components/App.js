1e255ca523b8c97c8744663d6d25ee135279560b
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2019-02-06 18:45:24 +0100

    Add material-ui
    - Version 3.9.2
    - Create nav bar with options

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 import styled from 'styled-components';
 
-const StyledLink = styled(Link)`
-  color: palevioletred;
-  font-weight: bold;
+const Base = styled.div`
+  flex-grow: 1;
+`;
+
+const Grow = styled.div`
+  flex-grow: 1
 `;
 
 const Index = () => <h2>Home</h2>;
 const About = () => <h2>About</h2>;
 const Users = () => <h2>Users</h2>;
+const SignUp = () => <h2>Sign up</h2>;
+const SignIn = () => <h2>Sign in</h2>;
 
-export default class App extends React.Component {
+class App extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -21,25 +29,42 @@ export default class App extends React.Component {
 
   render() {
     const { title } = this.state;
+    const IndexLink = props => <Link to="/" {...props} />;
+    const AboutLink = props => <Link to="/about/" {...props} />;
+    const UsersLink = props => <Link to="/users/" {...props} />;
+    const SignUpLink = props => <Link to="/signup/" {...props} />;
+    const SignInLink = props => <Link to="/signin/" {...props} />;
+
     return (
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <StyledLink to="/">{ title }</StyledLink>
-            </li>
-            <li>
-              <StyledLink to="/about/">About</StyledLink>
-            </li>
-            <li>
-              <StyledLink to="/users/">Users</StyledLink>
-            </li>
-          </ul>
-        </nav>
+      <Base>
+        <AppBar position="static">
+          <Toolbar>
+            <Button component={IndexLink} color="inherit">
+              { title }
+            </Button>
+            <Button component={AboutLink} color="inherit">
+                About
+            </Button>
+            <Button component={UsersLink} color="inherit">
+               Users
+            </Button>
+            <Grow />
+            <Button component={SignUpLink} color="inherit">
+               Sign Up
+            </Button>
+            <Button component={SignInLink} color="inherit">
+               Sign In
+            </Button>
+          </Toolbar>
+        </AppBar>
         <Route path="/" exact component={Index} />
         <Route path="/about/" component={About} />
         <Route path="/users/" component={Users} />
-      </div>
+        <Route path="/signup/" component={SignUp} />
+        <Route path="/signin/" component={SignIn} />
+      </Base>
     );
   }
 }
+
+export default App;
