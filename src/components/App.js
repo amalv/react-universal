290644376a87ea661d8f0fc89654480a22d7bbb8
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2019-02-19 16:11:06 +0100

    Add Routes component to App component
    - Simplifies routing handling
    - Use switch for managing routes by priority

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,8 @@
 // @flow
 import React from "react";
-import { Route } from "react-router-dom";
 import styled from "styled-components";
-import LogOut from "./LogOut";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
-import Home from "./Home";
-import About from "./About";
-import Users from "./Users";
 import Header from "./Header";
+import Routes from "./Routes";
 
 const Base = styled.div`
   flex-grow: 1;
@@ -17,12 +11,7 @@ const Base = styled.div`
 const App = () => (
   <Base>
     <Header />
-    <Route path="/" exact component={Home} />
-    <Route path="/about/" component={About} />
-    <Route path="/users/" component={Users} />
-    <Route path="/signup/" component={SignUp} />
-    <Route path="/signin/" component={SignIn} />
-    <Route path="/logout/" component={LogOut} />
+    <Routes />
   </Base>
 );
 
