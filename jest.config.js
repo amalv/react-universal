b702ff9a69c60b2ae225e6fe462f84b570b8021a
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2019-03-01 16:27:49 +0100

    Exclude cypress folder from jest

--- jest.config.js
+++ jest.config.js
@@ -150,9 +150,7 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all test paths, matched
   // tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: ["/node_modules/", "/cypress/"],
 
   // The regexp pattern Jest uses to detect test files
   // testRegex: "",
